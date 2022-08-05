467a9e384ba1220b1ebf0e7d96fee094d78e5d78
Peter Morris <mrpmorris@gmail.com>
2022-08-05 21:43:38 +0100

    Start tutorial 1

--- Source/Tutorials/01-MixInAMethod/MixInAMethod/Program.cs
+++ Source/Tutorials/01-MixInAMethod/MixInAMethod/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MixInAMethod;
+
 Console.WriteLine("Hello, World!");
+
+// TODO
+//var person = new Person();
+//person.SayHello();
