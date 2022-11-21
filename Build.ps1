71031b68ea20b27107b8a8d9a6ba8ae0c3857083
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-21 12:55:45 +0000

    Back to projectReference now it works with the other chagnes to the Moxy.csproj :)
    so we don't need the nuget.config nor do we need to prebuild Moxy

--- Build.ps1
+++ Build.ps1
@@ -2,5 +2,4 @@ dotnet tool restore
 Push-Location .\Source
 dotnet cleanup -y
 Pop-Location
-dotnet build .\Source\Lib\Morris.Moxy\Morris.Moxy.csproj
 dotnet build .\Source\Morris.Moxy.sln
--- Release.ps1
+++ Release.ps1
@@ -2,5 +2,4 @@ dotnet tool restore
 Push-Location .\Source
 dotnet cleanup -y
 Pop-Location
-dotnet build .\Source\Lib\Morris.Moxy\Morris.Moxy.csproj -c Release
 dotnet build .\Source\Morris.Moxy.sln -c Release
