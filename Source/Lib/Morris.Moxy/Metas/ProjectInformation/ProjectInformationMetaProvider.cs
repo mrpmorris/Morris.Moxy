ea80edc1cd84d8b7d9e84ed129dacbc62bb3c111
Peter Morris <mrpmorris@gmail.com>
2023-05-18 11:49:20 +0100

    Refactor

--- Source/Lib/Morris.Moxy/Metas/ProjectInformation/ProjectInformationMetaProvider.cs
+++ Source/Lib/Morris.Moxy/Metas/ProjectInformation/ProjectInformationMetaProvider.cs
@@ -7,17 +7,15 @@ internal static class ProjectInformationMetaProvider
 	public static IncrementalValueProvider<ProjectInformationMeta> CreateProjectInformationProvider(this IncrementalGeneratorInitializationContext context) =>
 		context
 			.AnalyzerConfigOptionsProvider
-			.Select(static (x, _) =>
-			{
-				x.GlobalOptions.TryGetValue("build_property.ProjectDir", out string? value);
-				return value ?? "";
-			})
+			.Select(static (x, _) => x.GlobalOptions.TryGetValue("build_property.ProjectDir", out string? value) ? value : "")
 			.Combine(context.AnalyzerConfigOptionsProvider)
 			.Select(static (x, _) =>
-			{
-				x.Right.GlobalOptions.TryGetValue("build_property.RootNamespace", out string? value);
-				return new ProjectInformationMeta(
+				x.Right.GlobalOptions.TryGetValue("build_property.RootNamespace", out string? value)
+				? new ProjectInformationMeta(
 					path: x.Left,
-					@namespace: value ?? "");
-			});
+					@namespace: value)
+				: new ProjectInformationMeta(
+					path: x.Left,
+					@namespace: "")
+			);
 }
