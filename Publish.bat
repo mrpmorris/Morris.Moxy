1b833199302ad6250d16d5682d0ad7eae13ecf93
Peter Morris <mrpmorris@gmail.com>
2023-05-23 16:21:00 +0100

    Update version

--- Publish.bat
+++ Publish.bat
@@ -1,5 +1,5 @@
 cls
-@echo **** 1.5.0-Beta1 : UPDATED THE VERSION NUMBER IN THE PROJECT *AND* BATCH FILE? ****
+@echo **** 1.5.0 : UPDATED THE VERSION NUMBER IN THE PROJECT *AND* BATCH FILE? ****
 pause
 
 cls
@@ -7,9 +7,9 @@ cls
 
 @echo ======================
 
-set /p ShouldPublish=Publish 1.5.0-Beta1 [yes]?
+set /p ShouldPublish=Publish 1.5.0 [yes]?
 @if "%ShouldPublish%" == "yes" (
 	@echo PUBLISHING
-	dotnet nuget push .\Source\Lib\Morris.Moxy\bin\Release\Morris.Moxy.1.5.0-Beta1.nupkg -k %MORRIS.NUGET.KEY% -s https://api.nuget.org/v3/index.json
+	dotnet nuget push .\Source\Lib\Morris.Moxy\bin\Release\Morris.Moxy.1.5.0.nupkg -k %MORRIS.NUGET.KEY% -s https://api.nuget.org/v3/index.json
 )
 
