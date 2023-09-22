87ffdccbc70bbbe928aee2dbe8e7f92d1c377d29
Peter Morris <mrpmorris@gmail.com>
2023-09-22 14:49:42 +0100

    Type.Name and FullName

---
+++ Source/Lib/Morris.Moxy/Metas/ScriptVariables/TypeVariable.cs
@@ -0,0 +1,15 @@
+﻿namespace Morris.Moxy.Metas.ScriptVariables;
+
+public readonly struct TypeVariable
+{
+	public readonly string Name;
+	public readonly string FullName;
+
+	public TypeVariable(string name, string fullName)
+	{
+		Name = name ?? throw new ArgumentNullException(nameof(name));
+		FullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+	}
+
+	public override string ToString() => Name;
+}
