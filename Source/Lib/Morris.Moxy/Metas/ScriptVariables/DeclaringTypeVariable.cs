e0b758ae7f4997b9e185c336094f7866a6aef228
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-14 11:56:45 +0700

    Create new Moxy Variable DeclaringTypeVariable

---
+++ Source/Lib/Morris.Moxy/Metas/ScriptVariables/DeclaringTypeVariable.cs
@@ -0,0 +1,11 @@
+﻿namespace Morris.Moxy.Metas.ScriptVariables;
+
+public readonly struct DeclaringTypeVariable
+{
+	public readonly string Name;
+
+	public DeclaringTypeVariable(string name)
+	{
+		Name = name;
+	}
+}
---
+++ Source/Tutorials/01-MixInAMethod/MixInAMethod/DeclaringTypeTest.cs
@@ -0,0 +1,7 @@
+namespace MixInAMethod;
+
+public partial class Container
+{
+	[DumpMoxy]
+	public partial class Nested {}		
+}
